a64ee31bf17138bfb6b967dcc07780b09cb68499
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-05-16 16:17:00 +0000

    07. Kruskal

---
+++ prg7.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+class prg7 {
+
+    static void kruskal(int[][] cost, int n) {
+        int[] par = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = -1;
+        }
+
+        int a = 0, b = 0, u = 0, v = 0, mincost = 0, ne = 0;
+
+        while (ne < n - 1) {
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (cost[i][j] < min) {
+                        min = cost[i][j];
+                        a = u = i;
+                        b = v = j;
+                    }
+                }
+            }
+
+            while (par[u] != -1) {
+                u = par[u];
+            }
+
+            while (par[v] != -1) {
+                v = par[v];
+            }
+
+            if (u != v) {
+                System.out.println("Cost of Laying road from town " + a + " to town " + b + " is " + min + " lakhs.");
+                mincost += min;
+                if (u < v) {
+                    par[v] = u;
+                } else {
+                    par[u] = v;
+                }
+                ne++;
+            }
+
+            cost[a][b] = cost[b][a] = Integer.MAX_VALUE;
+        }
+
+        System.out.println("Cost of Laying entire road network = " + mincost);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no of towns: ");
+        int n = sc.nextInt();
+
+        int[][] cost = new int[n][n];
+        System.out.println("Enter the cost of Laying roads between two towns in lakhs: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cost[i][j] = sc.nextInt();
+                if (cost[i][j] == 0) {
+                    cost[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        kruskal(cost, n);
+        sc.close();
+    }
+}
